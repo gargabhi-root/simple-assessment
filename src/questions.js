af650867a486e8487adc746452a5ab7d55a38428
Abhinav Garg <63925600+gargabhi-root@users.noreply.github.com>
2022-05-24 21:37:57 +0530

    questions store

---
+++ src/questions.js
@@ -0,0 +1,19 @@
+export const questions = [
+  {
+    question: 'A line which cuts a pair of parallel lines is called',
+    options: ['Tangent', 'Chord', 'Traversal', 'Intersector'],
+    answer: 'Chord',
+  },
+  {
+    question:
+      'If a certain sum of money can become 5 times its principal in 10 years, then the rate of interest is',
+    options: ['20%', '30%', '40%', '50%'],
+    answer: '40%',
+  },
+  {
+    question:
+      'A shopkeeper purchases 15 mangoes for Rs. 10 and sells them at 10 mangoes for Rs. 15. Thus, he earns a profit of',
+    options: ['50%', '75%', '80%', '125%'],
+    answer: '80%',
+  },
+];
